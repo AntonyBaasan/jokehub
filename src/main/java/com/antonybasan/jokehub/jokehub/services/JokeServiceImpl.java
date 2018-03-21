4c89f13c9a2d1e3859f157c046ddf0bff1ea4064
Antony Baasandorj <antony.baasan@gmail.com>
2018-03-21 17:08:07 -0400

    add dad joke service

--- src/main/java/com/antonybasan/jokehub/jokehub/services/JokeServiceImpl.java
+++ src/main/java/com/antonybasan/jokehub/jokehub/services/JokeServiceImpl.java
@@ -1,15 +1,35 @@
 package com.antonybasan.jokehub.jokehub.services;
 
 import com.antonybasan.jokehub.jokehub.domain.Joke;
+import com.antonybasan.jokehub.jokehub.domain.icanhazdadjoke.IcanDadJoke;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public class JokeServiceImpl implements JokeService{
+public class JokeServiceImpl implements JokeService {
     @Override
     public Joke getRandomJoke() {
         Joke j = new Joke();
         j.setName("Test joke");
         j.setDescription("Very long description");
+        return j;
+    }
+
+    public Joke getJokeByType(String jokeType) {
+        if (jokeType == "dad") {
+            return getDadJoke();
+        }
+
+        return null;
+    }
+
+    private Joke getDadJoke() {
+        RestTemplate restTemplate = new RestTemplate();
+        IcanDadJoke dadJoke = restTemplate.getForObject("https://icanhazdadjoke.com/", IcanDadJoke.class);
+
+        Joke j = new Joke();
+        j.setName(dadJoke.getId());
+        j.setDescription(dadJoke.getJoke());
         return j;
     }
 }
